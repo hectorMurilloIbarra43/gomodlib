16ee8af2261e3422f7421d756d34502bdbda98bd
hectorMurilloIbarra43 <hectormurillo43@gmail.com>
2021-03-08 00:48:24 -0700

    commands can be installed by user using the go tool as follows:
    $go get github.com/hectorMurilloIbarra43/gomodlib/cmd/gomodlib-greeting
    $go get github.com/hectorMurilloIbarra43/gomodlib/cmd/gomodlib-saludo
    
    Go downloads, builds and installs cmd-name into the default location

---
+++ cmd/gomodlib-greeting/somefile.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "github.com/hectorMurilloIbarra43/gomodlib/lib1"
+
+func main() {
+	fmt.Println(lib1.Bar())
+}
---
+++ cmd/gomodlib-saludo/somefile.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hectorMurilloIbarra43/gomodlib/lib2"
+)
+
+func main() {
+	fmt.Println(lib2.Baz())
+}
